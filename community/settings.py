605f020fdb040fbff8ef699f3e1fef5d8e013e29
ci-mustafa <akbarimustafa64@gmail.com>
2024-06-02 17:55:28 +0200

    feat: add a location field to event model to store the location of the event

--- community/settings.py
+++ community/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'location_field.apps.DefaultConfig',
     'debug_toolbar',
     'phonenumber_field',
     'communityhub',
@@ -142,3 +143,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'communityhub.User'
+
